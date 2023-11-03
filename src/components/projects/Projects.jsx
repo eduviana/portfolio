078cc41c5fb7f7de956002e069d886c8a4d3f45d
eduviana <eduardoviana83@gmail.com>
2023-11-03 13:05:50 -0300

    modificacion en el componente Projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,36 +1,48 @@
+// 'use client'
 // import { items } from "@/data";
 // import styles from "./projects.module.css";
 // import Image from "next/image";
 // import Button from "../button/Button";
+// import { useInView } from "react-intersection-observer";
 
 // const Projects = () => {
 //   return (
 //     <div className={styles.projects}>
-//       {items.map((item) => (
-//         <div key={item.id} className={styles.project}>
-//           <div className={styles.imgContainer}>
-//             <Image
-//               className={styles.img}
-//               src={item.image}
-//               alt={item.title}
-//               fill={true}
-//             />
-//           </div>
-//           <div className={styles.content}>
-//             <span className={styles.source}>{`Fuente: ${item.source}`}</span>
-//             <h1 className={styles.title}>{item.title}</h1>
-//             <p className={styles.desc}>{item.desc}</p>
-//             <Button text="Ver más" url={`${item.id}`} />
-//           </div>
-//         </div>
-//       ))}
+//       {items.map((item) => {
+//         const [ref, inView] = useInView({
+//           triggerOnce: true,
+//         });
 
+//         return (
+//           <div
+//             key={item.id}
+//             ref={ref}
+//             className={`${styles.project} ${inView ? styles.inView : ""}`}
+//           >
+//             <div className={styles.imgContainer}>
+//               <Image
+//                 className={styles.img}
+//                 src={item.image}
+//                 alt={item.title}
+//                 fill={true}
+//               />
+//             </div>
+//             <div className={styles.content}>
+//               <span className={styles.source}>{`Fuente: ${item.source}`}</span>
+//               <h1 className={styles.title}>{item.title}</h1>
+//               <p className={styles.desc}>{item.desc}</p>
+//               <Button text="Ver más" url={`${item.id}`} />
+//             </div>
+//           </div>
+//         );
+//       })}
 //     </div>
 //   );
 // };
+
 // export default Projects;
 
-'use client'
+"use client";
 import { items } from "@/data";
 import styles from "./projects.module.css";
 import Image from "next/image";
@@ -40,34 +52,37 @@ import { useInView } from "react-intersection-observer";
 const Projects = () => {
   return (
     <div className={styles.projects}>
-      {items.map((item) => {
-        const [ref, inView] = useInView({
-          triggerOnce: true,
-        });
+      {items.map((item) => (
+        <Project key={item.id} item={item} />
+      ))}
+    </div>
+  );
+};
 
-        return (
-          <div
-            key={item.id}
-            ref={ref}
-            className={`${styles.project} ${inView ? styles.inView : ""}`}
-          >
-            <div className={styles.imgContainer}>
-              <Image
-                className={styles.img}
-                src={item.image}
-                alt={item.title}
-                fill={true}
-              />
-            </div>
-            <div className={styles.content}>
-              <span className={styles.source}>{`Fuente: ${item.source}`}</span>
-              <h1 className={styles.title}>{item.title}</h1>
-              <p className={styles.desc}>{item.desc}</p>
-              <Button text="Ver más" url={`${item.id}`} />
-            </div>
-          </div>
-        );
-      })}
+const Project = ({ item }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={`${styles.project} ${inView ? styles.inView : ""}`}
+    >
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src={item.image}
+          alt={item.title}
+          fill={true}
+        />
+      </div>
+      <div className={styles.content}>
+        <span className={styles.source}>{`Fuente: ${item.source}`}</span>
+        <h1 className={styles.title}>{item.title}</h1>
+        <p className={styles.desc}>{item.desc}</p>
+        <Button text="Ver más" url={`${item.id}`} />
+      </div>
     </div>
   );
 };
